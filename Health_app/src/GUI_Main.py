6e28752ab79f15a1a9f44b5faf61a5d9cedd689c
Brian-Chan98 <31705402+Brian-Chan98@users.noreply.github.com>
2018-12-01 16:48:23 -0500

    Updated GUI_Main.py
    added comments to enhance readibility and clarity

--- Health_app/src/GUI_Main.py
+++ Health_app/src/GUI_Main.py
@@ -3,6 +3,7 @@ Created on 2018 M12 1
 
 @author: brian
 ''' 
+#imports needed methods from libraries
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -11,14 +12,16 @@ from kivy.uix.checkbox import CheckBox
 from kivy.core.window import Window
 from kivy.uix.button import Button
 
+#sets background colour to dark grey
+Window.clearcolor = (.2, .2, .2, 1) 
 
-Window.clearcolor = (.2, .2, .2, 1) #sets background colour to dark grey
 class Health(GridLayout):
     def __init__(self, **kwargs):
+        #constructor calls the super class and names it health
         super(Health, self).__init__(**kwargs)
         
         
-        
+        #first layout of buttons and labels for the alert tab
         layout = GridLayout(cols=2, row_force_default=True, row_default_height=40)
         layout.add_widget(CheckBox(active = False))
         layout.add_widget(Label(text = "Water"))
@@ -39,11 +42,14 @@ class Health(GridLayout):
         layout.add_widget(Label(text = "Rest your eyes"))
         layout.add_widget(Label())
        
-
+        
+        #adding the first layout to the Title Alert
         layout2 = GridLayout(rows=2, row_force_default=True, row_default_height=40)
         layout2.add_widget(Label(text = "Alerts", font_size = 50, bold = True, shorten = True))
         layout2.add_widget(layout)
        
+    
+        #3rd grid has the layout of the buttons and labels for the work tab
         layout3 = GridLayout(cols=2, row_force_default=True, row_default_height=40) 
         layout3.add_widget(Label(text = "Blocked Websites (seperated by comma)"))
         self.blocked = TextInput(multiline = False)
@@ -58,6 +64,8 @@ class Health(GridLayout):
         self.long = TextInput(multiline = False)
         layout3.add_widget(self.long)
         
+        
+        #piecing all the components to one frame
         self.rows = 5
         self.add_widget(layout2)
         self.add_widget(Label())
@@ -68,7 +76,7 @@ class Health(GridLayout):
         
     
 class HealthApp(App):
-    
+    #health App class takes App component and calls Health() function from build
     def build(self):
         return Health()
 
